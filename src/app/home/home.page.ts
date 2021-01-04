bce7f99ac979660df13bc3d0c0145accc68bf112
linhdangvu <35067520+linhdangvu@users.noreply.github.com>
2021-01-04 22:24:05 +0100

    seconde

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  data: any;
 
-  constructor() {}
+  constructor() { }
+
+  ngOnInit() {
+    fetch('assets/data/data.json').then(res => res.json())
+        .then(json => {
+          this.data = json;
+        });
+  }
+
+
 
 }
